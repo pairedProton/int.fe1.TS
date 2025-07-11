8fe3f762eebb12130b766f9e133526e76ad67f62
vinay <thevpspro@gmail.com>
2025-07-11 11:46:13 +0530

    ts code

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import Navbar from "./components/Navbar.tsx";
-import About from "./components/About.tsx";
-import Services from "./components/Services.tsx";
-import Blog from "./components/Blog.tsx";
-import Development from "./components/Development.tsx";
-import Contact from "./components/Contact.tsx";
+import About from "./sections/About.tsx";
+import Services from "./sections/Services.tsx";
+import Blog from "./sections/Blog.tsx";
+import Development from "./sections/Development.tsx";
+import Contact from "./sections/Contact.tsx";
 
 export default function App(): React.JSX.Element {
   return (
